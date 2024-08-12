70b73aa58ccce75b2611bbcd44f0828c180e62be
DiffTech <167343364+DiffTech@users.noreply.github.com>
2024-08-12 19:15:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// Change nav background on scroll
+// Change nav background on scroll and show logo
 window.addEventListener('scroll', function () {
     const nav = document.querySelector('nav');
+    const logo = document.querySelector('nav ul li.logo');
+    
     if (window.scrollY > 100) {
+        nav.classList.add('scrolled');
         nav.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
     } else {
+        nav.classList.remove('scrolled');
         nav.style.backgroundColor = 'rgba(0, 0, 0, 0)';
     }
 });
